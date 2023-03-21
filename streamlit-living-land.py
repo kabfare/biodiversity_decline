9d9c8e21c6d848631cb519f005d382f6e97b72b0
Kate Aviles-Bishop <kateberry993@gmail.com>
2023-03-21 18:41:45 -0400

    cleaned up code, added markdown comments, renamed xlsx files for clarity

--- streamlit-living-land.py
+++ streamlit-living-land.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import altair as alt
+
+
 
 st.title('Global Biodiversity Decline')
+
+st.write(' ')
+st.write(' ')
+st.write(' ')
 
 live=pd.read_excel('living-planet-spread.xlsx')
 
@@ -21,12 +28,23 @@ if st.checkbox('Show Raw Biodiversity Data'):
 
 chart=pd.DataFrame(live2, columns=['Africa', 'Asia and Pacific', 'Europe and Central Asia', 'Latin America and the Carribean', 'North America', 'World'])
 
-st.line_chart(chart)
+fig, ax=plt.subplots(figsize=(12,6))
+ax.plot(chart)
+ax.set(xlabel='Year', ylabel='Index (%)')
+ax.legend(['Africa', 'Asia', 'Europe', 'South America', 'North America'])
+st.pyplot(fig)
 
-st.caption('Above is a graph plotting the average index of biodiversity per region. Note that all regions are on a steady decline.')
+st.caption('Above is a graph plotting the average index of biodiversity per region. Note that all regions are on a steady decline, particularly Latin America which has a sharper decline than all other regions. One possible cause of this could be deforestation related to farming. See the below graph.')
+
+st.write(' ')
+st.write(' ')
+st.write(' ')
 
 
-land=pd.read_excel('land_data.xlsx')
+
+
+land=pd.read_excel('fao_land_data_spread.xlsx')
+land=land.set_index('Year')
 
 st.subheader('Regional Increase in Land Use for Farming by Year')
 
@@ -35,6 +53,26 @@ if st.checkbox('Show Raw Land Area Data'):
     st.write(land)
     st.caption('Data Source: UNData')
 
-chart2=pd.DataFrame(land, columns=['Africa', 'Asia', 'Europe', 'South America', 'North America', 'World'])
+chart2=pd.DataFrame(land, columns=['Africa', 'Asia', 'Europe', 'South America', 'North America'])
+chart3=pd.DataFrame(land, columns=['World'])
 
-st.line_chart(chart2)
+fig, ax=plt.subplots(figsize=(12,6))
+ax.plot(chart2)
+ax.set(xlabel='Year', ylabel='Area (1000 Ha)e+06')
+ax.legend(['Africa', 'Asia', 'Europe', 'South America', 'North America'])
+st.pyplot(fig)
+
+st.caption('Above is a graph plotting the area of farmland used per region. The area is shown as 1000 Ha, with one thousand Ha equating to 2,471.05 acres. As you can see, the graphs line up well, with Europe seeing an increase in biodiversity in the 1990s as their lad devoted to farming decreased. Latin America does not see a very dramatic increase, though the introduction of invasive species of plants and animals can also be attributed to their decline.')
+
+st.write(' ')
+st.write(' ')
+st.write(' ')
+
+st.subheader('Global Increase in Land Use for Farming by Year')
+
+fig, ax=plt.subplots(figsize=(12,6))
+ax.plot(chart3)
+ax.set(xlabel='Year', ylabel='Area (1000 Ha)e+06')
+st.pyplot(fig)
+
+st.caption('I put the Global area of farmland in its own graph as the area was so extreme that it flattened the line graph of the individual regions. Note the heavy drop around the 1990s, around the same time that three regions had an increase in farmland.')

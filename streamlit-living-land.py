bb7730421c4a8c3e9f9aa2f4d4aa50721665d613
Kate Aviles-Bishop <kateberry993@gmail.com>
2023-03-23 16:00:45 -0400

    Moved around streamlit elements and added x & y axis info

--- streamlit-living-land.py
+++ streamlit-living-land.py
@@ -20,21 +20,17 @@ live2=pd.pivot_table(live, index='Year', columns='Region', values='Average Index
 
 st.subheader('Decline of Average Index by Year')
 
-if st.checkbox('Show Raw Biodiversity Data'):
-    st.subheader('Raw Data')
-    st.write(live2)
-    st.caption("Data Source: World Wildlife Fund (WWF) and Zoological Society of London")
-
 chart=pd.DataFrame(live2, columns=['Africa', 'Asia and Pacific', 'Europe and Central Asia', 'Latin America and the Carribean', 'North America', 'World'])
 
 st.line_chart(chart)
 
-st.caption('Above is a graph plotting the average index (y-axis) of biodiversity per region. Note that all regions are on a steady decline, particularly Latin America which has a sharper decline than all other regions. One possible cause of this could be deforestation related to farming. See the below graph.')
+st.caption('x-axis: Year | y-axis: Index(%)')
+
+st.write('Above is a graph plotting the average index percentage of biodiversity per region. Note that all regions are on a steady decline, particularly Latin America which has a sharper decline than all other regions. One possible cause of this could be deforestation related to farming. See the below graph.')
 
 st.write(' ')
 st.write(' ')
 st.write(' ')
-
 
 
 
@@ -43,17 +39,14 @@ land=land.set_index('Year')
 
 st.subheader('Regional Increase in Land Use for Farming by Year')
 
-if st.checkbox('Show Raw Land Area Data'):
-    st.subheader('Raw Data')
-    st.write(land)
-    st.caption('Data Source: UNData')
-
 chart2=pd.DataFrame(land, columns=['Africa', 'Asia', 'Europe', 'South America', 'North America'])
 chart3=pd.DataFrame(land, columns=['World'])
 
 st.line_chart(chart2)
 
-st.caption('Above is a graph plotting the area of farmland used per region. The area (y-axis) is shown in Hectare (Ha), with 1Ha equating to 2.47 acres. As you can see, the graphs line up well, with Europe seeing an increase in biodiversity in the 1990s as their land devoted to farming decreased. Latin America does not see a very dramatic increase, though the introduction of invasive species of plants and animals can also be attributed to their decline.')
+st.caption('x-axis: Year | y-axis: Area(Ha)')
+
+st.write('Above is a graph plotting the area of farmland used per region. The area (y-axis) is shown in Hectare (Ha), with 1Ha equating to 2.47 acres. As you can see, the graphs line up well, with Europe seeing an increase in biodiversity in the 1990s as their land devoted to farming decreased. Latin America does not see a very dramatic increase, though the introduction of invasive species of plants and animals can also be attributed to their decline.')
 
 st.write(' ')
 st.write(' ')
@@ -63,4 +56,20 @@ st.subheader('Global Increase in Land Use for Farming by Year')
 
 st.line_chart(chart3)
 
-st.caption('I put the Global area of farmland in its own graph as the area was so extreme that it flattened the line graph of the individual regions. Note the heavy drop around the 1990s, around the same time that four of the five regions had an increase in farmland usage.')
+st.caption('x-axis: Year | y-axis: Area(Ha)')
+
+st.write('The Global area of farmland is in its own graph as the area was so extreme that it flattened the line graph of the individual regions. Note the heavy drop around the 1990s, around the same time that four of the five regions had an increase in farmland usage.')
+
+st.write(' ')
+st.write(' ')
+st.write(' ')
+
+if st.checkbox('Show Raw Land Area Data'):
+    st.subheader('Raw Data')
+    st.write(land)
+    st.caption('Data Source: UNData')
+
+if st.checkbox('Show Raw Biodiversity Data'):
+    st.subheader('Raw Data')
+    st.write(live2)
+    st.caption("Data Source: World Wildlife Fund (WWF) and Zoological Society of London")
